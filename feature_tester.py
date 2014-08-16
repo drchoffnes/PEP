467a26526115a43c7170c0880e5e4c0833f8e0f2
xingx <xingx@chromium.org>
2014-08-15 22:35:00 -0700

    first commit

---
+++ feature_tester.py
@@ -0,0 +1,121 @@
+import httplib, os, string, random, time, socket
+
+SERVER = "128.125.121.204"
+
+def fprint(s):
+	f_out.write(s + "\r\n")
+	print s
+
+# start to log TCP traces on |SERVER|, with filename |logfile|
+def start_tcpdump(logfile):
+	request(SERVER + ":9888", fileobject="tcpdump?start&" + logfile)
+	return
+
+# stop to log TCP traces |logfile|, and scp |logfile| from |SERVER| to local |local_filename|
+def stop_tcpdump(logfile):
+	request(SERVER + ":9888", fileobject="tcpdump?stop&" + logfile)
+	time.sleep(2)
+	os.system("wget " + SERVER + "/PEP/" + logfile + " -q -O " + label + "/" + logfile)
+	if os.path.getsize(label + "/" + logfile) == 0:
+		fprint("\t trace file retrieving fails.")
+	return
+
+def test_caching():
+	fprint("Caching:")
+	logfile = "log_c_" + label + ".pcap"
+
+	start_tcpdump(logfile)
+
+	f = "test_cache_" + str(random_id) + ".jpg"
+	request(fileobject=f)
+	time.sleep(3)
+	request(fileobject=f)
+
+	stop_tcpdump(logfile)
+	return
+
+def test_connection_persistence():
+	fprint("Connection Persistence:")
+	logfile = "log_cp_" + label + ".pcap"
+
+	start_tcpdump(logfile)
+	f = "non_exist_file_" + str(random_id) + ".jpg"
+	request(fileobject=f, extra_headers={"Connection": "close"})
+	stop_tcpdump(logfile)
+
+# testing whether downloaded html is from our server, or carrier-choosed "www.cnn.com"
+def test_redirection():
+	fprint("Redirection:")
+	request(hostname="www.cnn.com", saveto=label + "/redirection")
+	if open(label + "/redirection").read() != "Fake!!!\n":
+		fprint("\tredirecting!")
+
+# testing whether TCP handshake will be delayed
+def test_delayed_handshake():
+	fprint("Delayed Handshaking:")
+	logfile = "log_dh_" + label + ".pcap"
+	start_tcpdump(logfile)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((SERVER, 80))
+	time.sleep(3)
+	s.send("GET /non_exist_file_" + str(random_id) + " HTTP/1.0\r\nHost: 128.125.121.204\r\nAccept-Encoding: identity\r\n\r\n")
+	data = s.recv(1)
+	stop_tcpdump(logfile)
+
+# testing whether downloaded jpg, css, js, html files are of the same size of origins.
+def test_contentrewriting():
+ 	fprint("Content Rewriting:")
+	request(fileobject="test.jpg", saveto=label + "/jpg")
+	if os.path.getsize(label + "/jpg") != 211258:
+		fprint("\timage transcoding!")
+
+	request(fileobject="test.css", saveto=label + "/css")
+	if os.path.getsize(label + "/css") != 3999:
+		fprint("\tcss transcoding!")
+
+	request(fileobject="test.js", saveto=label + "/js")
+	if os.path.getsize(label + "/js") != 7999:
+		fprint("\tjavascript transcoding!")
+
+	request(fileobject="test.html", saveto=label + "/html")
+	if os.path.getsize(label + "/html") != 3999:
+		fprint("\thtml transcoding!")
+
+# Make a HTTP GET request to |ip|, with header Host field |hostname|, other headers |extra_headers|, the requested object is |fileobject|. Response will be saved to local file |saveto|.
+def request(ip=SERVER, hostname="", fileobject="", saveto="temp", extra_headers=None):
+	if extra_headers == None:
+		extra_headers = {}
+	web = httplib.HTTPConnection(ip)
+	headers = extra_headers
+	print headers
+	if len(hostname) > 0:
+		headers["Host"] = hostname
+	headers["User-Agent"] = "Mozilla/5.0 (Linux; Android 4.2; Nexus 7 Build/JOP40C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19"
+	web.request("GET", "/" + fileobject, headers=headers)
+	response = web.getresponse()
+  # save to file |saveto|
+	f = open(saveto, "w")
+	f.write(response.read())
+
+def test():
+	test_redirection()
+	test_contentrewriting()
+	test_caching()
+	test_connection_persistence()
+	test_delayed_handshake()
+
+if __name__ == "__main__":
+	random.seed()
+	os.system("sudo echo")
+	print "Please only input [A-Za-z0-9] for below carrier & location info...\n"
+	carrier = raw_input('\tInput experiment carrier:  ')
+	location = raw_input('\tInput experiment location: ')
+	
+	random_id = random.randint(1, 1000)
+	label = "PEP_" + carrier + "_" + location + "_" + str(random_id)
+	os.system("mkdir " + label)
+	f_out = open(label + "/result.txt", "w")
+	test()
+	keep = raw_input('\nDo you want to keep this experiment result? [y/n]')
+	if keep != 'y':
+		os.system("rm -rf " + label)
---
+++ tcpdump_slave.py
@@ -0,0 +1,74 @@
+import os, sys, threading, errno, signal, subprocess, time, BaseHTTPServer, urlparse, cgi,urllib, urllib2, time, copy
+from multiprocessing import Process
+from SocketServer import ThreadingMixIn
+
+HOST_NAME = '128.125.121.204'
+HOST_PORT = 8888
+
+tcpdumps = {}
+tcpdump_cleaner_run = True
+def tcpdump_cleaner():
+	while(tcpdump_cleaner_run):
+		time.sleep(10)
+		to_kill = []
+		tcpdumps_ = copy.deepcopy(tcpdumps)
+		for x in tcpdumps_:
+			if int(time.time()) - tcpdumps[x] > 300:
+				kill_tcpdump(x)
+				to_kill.append(x)
+
+def tcpdump_on_another_process(filename):
+	os.system("tcpdump -s0 -v port 80 -q -w " + filename)
+
+def kill_tcpdump(filename):
+	p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+	out, err = p.communicate()
+
+	for line in out.splitlines():
+		if filename in line:
+			pid = int(line.split(None, 2)[1])
+			os.system("kill " + str(pid))
+	del tcpdumps[filename]
+
+def start_log(filename):
+	tcpdumps[filename] = int(time.time())
+	p = Process(target=tcpdump_on_another_process, args=(filename,))
+	p.start()
+	
+def stop_log(filename):
+	kill_tcpdump(filename)
+	os.system("mv " + filename + " /var/www/PEP/")
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	def do_GET(self):
+		url_content = urlparse.urlparse(self.path)
+		query = url_content.query
+		path = url_content.path
+		if path != "/tcpdump":
+			return
+		args = query.split("&")
+
+		if args[0] == "start":
+			start_log(args[1])
+			time.sleep(2)
+		else:
+			stop_log(args[1])
+		self.send_response(200)
+		self.end_headers()
+		self.wfile.write("")
+
+class ThreadedHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+	"""Handle requests in a separate thread."""
+
+if __name__ == '__main__':
+	thread = threading.Thread(target=tcpdump_cleaner)
+	server = ThreadedHTTPServer((HOST_NAME, HOST_PORT), MyHandler)
+
+        try:
+		thread.start()
+                server.serve_forever()
+        except KeyboardInterrupt:
+                pass
+
+	tcpdump_cleaner_run = False
+
